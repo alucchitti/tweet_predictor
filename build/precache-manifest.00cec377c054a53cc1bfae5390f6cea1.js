a33e6f8b899ee8362e53bbdd61ba1cfe555de80f
alucchitti <ael74@pitt.edu>
2021-08-03 15:09:33 -0400

    Edit deploy settings

---
+++ build/precache-manifest.00cec377c054a53cc1bfae5390f6cea1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a37e8b4f949be40a5ae4d8f91c63d5a",
+    "url": "/tweet_predictor/index.html"
+  },
+  {
+    "revision": "b9842ac9e591efb03545",
+    "url": "/tweet_predictor/static/css/main.a33a60fd.chunk.css"
+  },
+  {
+    "revision": "b7129d48abb049377a10",
+    "url": "/tweet_predictor/static/js/2.d9648712.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "/tweet_predictor/static/js/2.d9648712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00205b86fca7073f718",
+    "url": "/tweet_predictor/static/js/3.a1d6e309.chunk.js"
+  },
+  {
+    "revision": "b9842ac9e591efb03545",
+    "url": "/tweet_predictor/static/js/main.f31a188d.chunk.js"
+  },
+  {
+    "revision": "a18c29ae93825e4308b6",
+    "url": "/tweet_predictor/static/js/runtime-main.dc5584c6.js"
+  },
+  {
+    "revision": "6c3d710e781afed44861da0259753d7b",
+    "url": "/tweet_predictor/static/media/khloe.6c3d710e.jpg"
+  },
+  {
+    "revision": "5fcb184881d6e6a97f64ac872d4a4554",
+    "url": "/tweet_predictor/static/media/obama.5fcb1848.jpg"
+  }
+]);
